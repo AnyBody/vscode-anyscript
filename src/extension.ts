2df441a5e1001e855fcfc20ffc377d48505ca772
bke <bke@anybodytech.com>
2024-04-04 20:07:11 +0200

    disable dev command & logging

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { stringRGBToColor } from "./helpers";
 import { rgbColorPresentation, rgbMemberColorProvider } from "./intellisense/colorProvider";
 
 // This method is called when your extension is activated
@@ -9,21 +8,21 @@ import { rgbColorPresentation, rgbMemberColorProvider } from "./intellisense/col
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "anyscript" is now active!');
+  // console.log('Congratulations, your extension "anyscript" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "anyscript.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from AnyScript!");
-    }
-  );
+  // let disposable = vscode.commands.registerCommand(
+  //   "anyscript.helloWorld",
+  //   () => {
+  //     // The code you place here will be executed every time your command is executed
+  //     // Display a message box to the user
+  //     vscode.window.showInformationMessage("Hello World from AnyScript!");
+  //   }
+  // );
 
-  context.subscriptions.push(disposable);
+  // context.subscriptions.push(disposable);
 
   // // Register a completion item provider for the new language
   // const provider = vscode.languages.registerCompletionItemProvider('anyscript', {
